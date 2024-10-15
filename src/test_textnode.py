9ed672bcb707fdffdf4e7951124b20c12164846a
Alexander-Plunkett <alexander.b.plunkett@gmail.com>
2024-10-15 16:12:47 +0100

    added unit testing

---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_noeq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is also a text node", TextType.BOLD)
+        self.assertNotEqual(node.text, node2.text)
+
+    def test_noeq_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node.text_type, node2.text_type)
+
+    def test_noeq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        self.assertNotEqual(node.url, node2.url)
+        
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,13 +1,20 @@
+from enum import Enum
+
+class TextType(Enum):
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 class TextNode():
-    def __init__(self, text, type, url):
+    def __init__(self, text, type, url = None):
         self.text = text
-        self.text_type = type
+        self.text_type = TextType(type)
         self.url = url
 
-    def __eq__(self, comparison):
-        if self.text == comparison.text and self.text_type == comparison.text_type and self.url == comparison.url:
-            return True
-        return False
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
